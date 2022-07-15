995f84d4e616d27ce15370be8c1a9b8f74b46b69
JoKyungseo <eksql0645@gmail.com>
2022-07-15 11:37:00 +0900

    [Feat] 모델 생성 및 모델 간 관계 설정
    db 객체에 정의한 모델 객체 할당
    테이블과 모델 연결
    모델 간 관계 정립

--- models/index.js
+++ models/index.js
@@ -1,10 +1,15 @@
 const Sequelize = require('sequelize');
+const config = require('../config/config.json')[env];
+const User = require('./user');
+const Calendar = require('./calendar');
+const WorkInfo = require('./workInfo');
+const WorkTag = require('./workTag');
 
 const env = process.env.NODE_ENV || 'development';
-const config = require('../config/config.json')[env];
+
 const db = {};
 
-// db 설정을 가진 MySQL 연결 객체 생성
+// db 설정을 가진 MySQL 연결 객체(sequelize) 생성
 const sequelize = new Sequelize(
   config.database,
   config.username,
@@ -12,6 +17,40 @@ const sequelize = new Sequelize(
   config
 );
 
+// db 객체에 sequelize 및 모델 추가
 db.sequelize = sequelize;
+db.User = User;
+db.WorkInfo = WorkInfo;
+db.WorkTag = WorkTag;
+db.Calendar = Calendar;
+
+// 각 모델의 init 호출: 테이블과 모델 연결
+User.init(sequelize);
+Calendar.init(sequelize);
+WorkInfo.init(sequelize);
+WorkTag.init(sequelize);
+
+// 테이블 간 관계 설정
+User.associate(db);
+Calendar.associate(db);
+WorkInfo.associate(db);
+WorkTag.associate(db);
+
+db.User = User;
+db.Calendar = Calendar;
+db.WorkInfo = WorkInfo;
+db.WorkTag = WorkTag;
+
+// 각 모델 init 메서드 호출 -> 테이블과 모델 연결
+User.init(sequelize);
+Calendar.init(sequelize);
+WorkInfo.init(sequelize);
+WorkTag.init(sequelize);
+
+// 다른 테이블과의 관계 연결
+User.associate(db);
+Calendar.associate(db);
+WorkInfo.associate(db);
+WorkTag.associate(db);
 
 module.exports = db;
