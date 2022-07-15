0c857cd13536a9ce2750b2ede74f4b5ecde8297b
JoKyungseo <eksql0645@gmail.com>
2022-07-15 11:36:37 +0900

    [Feat] Calendar 모델 추가
    시퀄라이즈로 calendars 테이블과 Calendar 모델 연결

---
+++ models/calendar.js
@@ -0,0 +1,38 @@
+const Sequelize = require('sequelize');
+
+module.exports = class Calendar extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        work_day: { type: Sequelize.DATEONLY, allowNull: false },
+        unit: { type: Sequelize.FLOAT, allowNull: true },
+        memo: { type: Sequelize.STRING(100), allowNull: true },
+        color: {
+          type: Sequelize.STRING(10),
+          allowNull: true,
+          defaultValue: '#d7e4f7',
+        },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        underscored: false,
+        modelName: 'Calendar',
+        tableName: 'calendars',
+        paranoid: false,
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+      }
+    );
+  }
+  // 테이블 간 관계 정립
+  static associate(db) {
+    db.Calendar.belongTo(db.User, {
+      foreignKey: 'cal_worker',
+      targetKey: 'id',
+    });
+    db.Calendar.belongToMany(db.WorkTag, {
+      through: 'CalendarWorkTag',
+    });
+  }
+};
