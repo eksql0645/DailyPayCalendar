cf6807d2a89fb33928090788a80616df54d7d159
JoKyungseo <eksql0645@gmail.com>
2022-07-15 11:36:44 +0900

    [Feat] WorkInfo 모델 추가
    시퀄라이즈로 workInfos 테이블과 WorkInfo 모델 연결

---
+++ models/workInfo.js
@@ -0,0 +1,34 @@
+const Sequelize = require('sequelize');
+
+module.exports = class User extends Sequelize.Model {
+  static init(sequelize) {
+    return super.init(
+      {
+        initial_pay: {
+          type: Sequelize.INTEGER,
+          allowNull: false,
+        },
+        initial_unit: { type: Sequelize.FLOAT, allowNull: false },
+        work_start: { type: Sequelize.DATEONLY, allowNull: false },
+        work_end: { type: Sequelize.DATEONLY, allowNull: false },
+      },
+      {
+        sequelize,
+        timestamps: true,
+        underscored: false,
+        modelName: 'WorkInfo',
+        tableName: 'workInfos',
+        paranoid: false,
+        charset: 'utf8',
+        collate: 'utf8_general_ci',
+      }
+    );
+  }
+  // 테이블 간 관계 정립
+  static associate(db) {
+    db.WorkInfo.belongTo(db.User, {
+      foreignKey: 'work_worker',
+      sourceKey: 'id',
+    });
+  }
+};
