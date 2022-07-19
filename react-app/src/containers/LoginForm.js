2405237cc7685622815970102ba0601d552ec4c5
JoKyungseo <eksql0645@gmail.com>
2022-07-19 17:22:38 +0900

    [Comment] LoginForm 코드 설명 추가

--- react-app/src/containers/LoginForm.js
+++ react-app/src/containers/LoginForm.js
@@ -5,12 +5,16 @@ import { changeField, initializeForm } from '../modules/auth';
 
 function LoginForm() {
   const dispatch = useDispatch();
+
+  // store 안에 state 중 auth를 선택 -> auth.login을 form에 할당
+  // {form: {email: '', password:''}}여서 {form} 함
   const { form } = useSelector(({ auth }) => ({
     form: auth.login,
   }));
 
   const onChange = (e) => {
     const { value, name } = e.target;
+    // 리듀서 호출하여 action(요구사항)대로 스토어에 있는 auth.login의 상태 변경
     dispatch(
       changeField({
         form: 'login',
@@ -24,6 +28,7 @@ function LoginForm() {
     e.preventDefault();
   };
 
+  // 마운트될 때만 실행된다. 로그인input을 초기화시킨다.
   useEffect(() => {
     dispatch(initializeForm('login'));
   }, [dispatch]);
