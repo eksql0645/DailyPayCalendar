588a70c79aa6d5249c5ce00a098a8bb8a33de764
JoKyungseo <eksql0645@gmail.com>
2022-07-19 17:42:29 +0900

    [Feat] loading module 추가
    redux-saga를 사용하여 API 요청 상태 관리를 더 간단히 하기 위해 laoding 모듈 추가

--- react-app/src/modules/index.js
+++ react-app/src/modules/index.js
@@ -1,8 +1,11 @@
 import { combineReducers } from 'redux';
 import auth from './auth';
+import loading from './loading';
 
+// reducer 추가
 const rootReducer = combineReducers({
   auth,
+  loading,
 });
 
 export default rootReducer;
